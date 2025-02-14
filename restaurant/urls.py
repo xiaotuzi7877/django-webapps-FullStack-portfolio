1149e79f869b931a238c21181e45e12db48df80c
xiaotuzi7877 <miclilzy@bu.edu>
2025-02-14 14:36:46 -0500

    clarify url

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('ho', views.main_page, name='main_page'),
+    path('', views.main, name = 'main'),
     path('main/', views.main, name = 'main'),
     path('order/', views.order, name = 'order'),
     path('confirmation/', views.confirmation, name = 'confirmation'),
