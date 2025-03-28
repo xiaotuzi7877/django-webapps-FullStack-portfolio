730151b2214048bac737de41da0460e411da0a8a
xiaotuzi7877 <miclilzy@bu.edu>
2025-03-28 15:21:11 -0400

    fix bugs of loging and posting status messages

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -10,7 +10,7 @@ Date: 02/21/2025
 
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import AddFriendView, ShowAllProfileView, ShowFriendSuggestionsView, ShowNewsFeedView, ShowProfilePageView, CreateProfileView, CreateStatusMessageView, UpdateProfileView, DeleteStatusMessageView, UpdateStatusMessageView, LogoutConfirmationView # Import both views
+from .views import AddFriendView, ShowAllProfileView, ShowFriendSuggestionsView, ShowMyProfileView, ShowNewsFeedView, ShowProfilePageView, CreateProfileView, CreateStatusMessageView, UpdateProfileView, DeleteStatusMessageView, UpdateStatusMessageView, LogoutConfirmationView # Import both views
 
 # URL patterns for the mini_fb application
 urlpatterns = [
@@ -24,11 +24,12 @@ urlpatterns = [
     path('profile/friend_suggestions/', ShowFriendSuggestionsView.as_view(), name='friend_suggestions'),
     path('profile/news_feed/', ShowNewsFeedView.as_view(), name='news_feed'),
     path('profile/add_friend/<int:pk>/<int:other_pk>/', AddFriendView.as_view(), name='add_friend'),
-    path('status/create_status/', CreateStatusMessageView.as_view(), name='create_status'),
-    path('profile/my/', ShowProfilePageView.as_view(), name='my_profile'),
+    path('status/create_status/<int:pk>/', CreateStatusMessageView.as_view(), name='create_status'),
+    path('profile/my/', ShowMyProfileView.as_view(), name='my_profile'),
 
     # Status message management
     path('status/<int:pk>/delete/', DeleteStatusMessageView.as_view(), name='delete_status'),
+    
     path('status/<int:pk>/update/', UpdateStatusMessageView.as_view(), name='update_status'),
 
     # Authentication
